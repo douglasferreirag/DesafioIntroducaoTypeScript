5bca806fa78637de34d6324a647929db3db01d5f
douglasferreirag <douglas.ferreirag@gmail.com>
2022-07-03 20:00:40 -0300

    Never and throw

---
+++ exemplos/any-unknown-never.ts
@@ -0,0 +1,40 @@
+let anyEstaDeVolta: any;
+
+anyEstaDeVolta = 3;
+
+anyEstaDeVolta = 'teste'
+
+anyEstaDeVolta = 5;
+
+let stringTeste: string = 'verificar'
+
+stringTeste = anyEstaDeVolta;
+
+let unknownValue: unknown;
+
+unknownValue = 3;
+
+unknownValue = 'opa';
+
+unknownValue = true;
+
+unknownValue = 'vai sim';
+
+
+let stringTest2: string = 'agora vai';
+
+
+
+if(typeof(unknownValue) === 'string')
+
+    stringTest2 = unknownValue
+
+
+function jogaErro(erro: string, codigo: number): never {
+
+        throw {error: erro, code: codigo}
+
+}
+
+jogaErro('deu erro', 500)
+
