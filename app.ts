25fe92cb98c2fd4209427cbe101574d15292a2ea
douglasferreirag <douglas.ferreirag@gmail.com>
2022-07-02 22:50:57 -0300

    any está de volta e sobre o unknown

--- app.ts
+++ app.ts
@@ -1,32 +1,34 @@
+let anyEstaDeVolta: any;
+
+anyEstaDeVolta = 3;
+
+anyEstaDeVolta = 'teste'
+
+anyEstaDeVolta = 5;
+
+let stringTeste: string = 'verificar'
+
+stringTeste = anyEstaDeVolta;
+
+let unknowValue: unknown;
+
+unknowValue = 3;
+
+unknowValue = 'opa';
+
+unknowValue = true;
+
+unknowValue = 'vai sim';
+
+
+let stringTest2: string = 'agora vai';
 
 
 
-function printaValoresNumericos(numero1: number, numero2: number):void{
+if(typeof(unknowValue) === 'string')
 
-    console.log(numero1 + numero2);
+    stringTest2 = unknowValue
 
 
-}
 
-function somarValoresNumericosETratar(numero1:number, numero2: number, callback:(numero: number) => number): number{
 
-    let resultado = numero1 + numero2;
-
-    return callback(resultado);
-
-}
-
-function aoQuadrado(numero: number): number{
-
-    return numero * numero;
-
-}
-
-function dividirPorEleMesmo(numero:number): number{
-
-        return numero/numero
-
-}
-
-console.log(somarValoresNumericosETratar(1,3,aoQuadrado))
-console.log(somarValoresNumericosETratar(1,3,dividirPorEleMesmo))
--- dist/8/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function somarValoresNumericos(numero1, numero2) {
-    return numero1 + numero2;
-}
-console.log(somarValoresNumericos(1, 3));
-function printaValoresNumericos(numero1, numero2) {
-    console.log(numero1 + numero2);
-}
