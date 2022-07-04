d8449abaa851bed8d6b8af8c12a68a8e575be3b5
douglasferreirag <douglas.ferreirag@gmail.com>
2022-07-03 23:02:06 -0300

    outDir e rootDir

---
+++ dist/exemplos/interrroga.js
@@ -0,0 +1,7 @@
+"use strict";
+let buttonTeste = document.getElementById("button");
+buttonTeste === null || buttonTeste === void 0 ? void 0 : buttonTeste.addEventListener('click', () => {
+    if (input1 && input2) {
+        console.log('Funcionou');
+    }
+});
